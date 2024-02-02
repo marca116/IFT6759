3eecd555ec12054a678324f69fdd95b664c48506
marca116 <marca116@hotmail.com>
2024-02-01 23:00:06 -0500

    init

---
+++ assistant_relation_extraction.py
@@ -0,0 +1,30 @@
+from openai_requests_utils import send_open_ai_gpt_message, read_json, format_msg_oai, extract_json_from_response
+
+prompt_config = read_json('prompts.json')
+convo_history = read_json('test_message_history/bill_fritz.json')
+
+# Add specification of which is the user msg and which is the history
+for idx, msg in enumerate(convo_history):
+    if idx < len(convo_history) - 1:
+        msg["content"] = "history: [" + msg["content"] + "]"
+    else:
+        msg["content"] = "Last user message: [" + msg["content"] + "]"
+
+# Insert the prompt intro
+convo_history.insert(0, format_msg_oai("system", prompt_config['relation_extractor_intro']))
+
+convo_history.insert(1, format_msg_oai("user", prompt_config['relations']))
+convo_history.insert(2, format_msg_oai("user", prompt_config['entities']))
+convo_history.insert(3, format_msg_oai("user", prompt_config['entities_subtypes']))
+# prompt_config = read_json('prompts_old.json')
+
+prompt = prompt_config['relation_extractor_body'] + prompt_config['relation_extractor_json'] + prompt_config['relation_extractor_conclusion']
+
+# Add specification person currently speaking is an entity
+# prompt += prompt_config["currently_speaking"]
+
+convo_history = convo_history + [format_msg_oai("user", prompt)]
+result = send_open_ai_gpt_message(convo_history, json_mode=True)
+
+obj_result = extract_json_from_response("relation_extraction", result["content"])
+print(obj_result)
