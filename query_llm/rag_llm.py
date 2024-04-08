79e6c152ba1949ca0579cde2429869b45edd28b9
oscar cuellar <oscarcuellar@oscars-mbp.home>
2024-04-07 21:17:00 -0600

    oai embeddings start

--- query_llm/rag_llm.py
+++ query_llm/rag_llm.py
@@ -8,6 +8,7 @@ import re
 import time
 from datetime import datetime
 
+from sklearn.neighbors import KDTree
 from evaluation import calc_question_f1_score, calc_question_macro_f1_score, get_final_solved_questions_obj
 from qa_utils import print_solved_question, sort_questions, process_question_with_entity_properties, process_question_with_rag_context
 
@@ -38,8 +39,52 @@ index_path = '/Users/oscarcuellar/ocn/mila/amlp/rag/data/models/wikipedia_kb_ind
 
 dataset = load_from_disk(passages_path)
 dataset.load_faiss_index("embeddings", index_path)
-#########################
+df = dataset.to_pandas()
 
+index_embs = np.array(df.embeddings.values.tolist())
+index_embs = np.divide(index_embs, np.linalg.norm(index_embs, axis=1)[:,np.newaxis])
+kd_index = KDTree(index_embs)
+titles = dict(zip(range(len(df)), df.title))
+#########################
+from openai import OpenAI
+client = OpenAI()
+
+if False:
+    oai_embeddings = dict()
+
+def get_embedding(obj, model="text-embedding-3-small"):
+    text = obj['text']
+    idx = obj['idx']
+    if not idx in oai_embeddings:
+        text = text.replace("\n", " ")
+        e = client.embeddings.create(input = [text], model=model).data[0].embedding
+        oai_embeddings[idx] = np.array(e)
+    return
+
+computed = set(oai_embeddings.keys())
+
+texts = [title + ' - ' + txt for title, txt in zip(df.title.tolist(), df.text.tolist())]
+texts = [{'text': t, 'idx': i} for i, t in enumerate(texts) if i not in computed]
+
+batch_size = 10
+start_time = time.time()
+
+# sepparate the data in groups
+batches = [texts[i:i + batch_size] for i in range(0, len(texts), batch_size)]
+
+# process each batch in parallel
+for i, batch in enumerate(batches):
+
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_embedding, txt) for txt in batch]
+
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Error occurred in sub-thread: {e}")
+    if i % 1000 == 0:
+        print(f"Processed {i + 1}/{len(batches)} batches")
 
 # LOAD MODELS ################
 ##############################
@@ -56,14 +101,9 @@ sys.path.insert(0, "../utils")
 from utils import clean_number, is_date, format_date_iso_format
 
 # qald_9_plus_train, qald_9_plus_train_with_long_answer, qald_10_test
-dataset_name = "qald_9_plus_train_with_long_answer"
+dataset_name = "qald_10_train"
 input_dataset_filename = "../datasets/" + dataset_name + "_final.json"
 output_filename = f'{dataset_name}_solved_answers.json'
-
-# Default answers when no entity linking is found
-no_extra_info_solved_answers_filename = f'{dataset_name}_solved_answers_no_external_info.json'
-with open(no_extra_info_solved_answers_filename, 'r', encoding='utf-8') as file:
-    question_answers_no_extra_info = json.load(file)
 
 # current date time format with fractions of seconds
 current_time = datetime.now().strftime("%Y%m%d-%H%M%S%f")
@@ -90,25 +130,48 @@ cos_sim = lambda a, b: np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 prompt_config = read_json('prompts.json')
 
-
+all_contexts = []
 def process_question_rag(question, rag_model):
-    global total_token_count, total_questions_with_tokens
+    global total_token_count, total_questions_with_tokens, all_contexts
 
-    encoded_question = rag_model.question_encoder(
+    encoded_question = rag_model.rag.question_encoder(
         **tokenizer.question_encoder(question['question'], return_tensors="pt")
     )[0].numpy()
 
-    indices, _ = rag_model.retriever.index.get_top_docs(encoded_question, 40)  # ITS NOT NECESSARILY PERFECTLY SORTED
+    #indices, _ = rag_model.retriever.index.get_top_docs(encoded_question, 100)  # ITS NOT NECESSARILY PERFECTLY SORTED
+    #contexts = sorted(
+    #    [(dataset[int(t)]['text'], cos_sim(dataset[int(t)]['embeddings'], encoded_question[0])) for t in indices[0]],
+    #    key=lambda k: -k[1]
+    #)[:10]
 
-    contexts = sorted(
-        [(dataset[int(t)]['text'], cos_sim(dataset[int(t)]['embeddings'], encoded_question[0])) for t in indices[0]],
-        key=lambda k: -k[1]
-    )[:10]
+    d, indices = kd_index.query(encoded_question, k=100)
+    contexts = [(dataset[int(t)]['text'], cos_sim(dataset[int(t)]['embeddings'], encoded_question[0])) for t in indices[0]]
 
-    contexts = [t[0] for t in contexts]
+    #all_contexts += contexts
+
+    contexts = [t[0] for t in contexts[:10]]
 
     answers, original_answers, reason, answers_datatype, extra_info, token_count = process_question_with_rag_context(
         question, contexts, info_messages_dir, prompt_config)
+
+
+    inputs_dict = rag_model.retriever.question_encoder_tokenizer.batch_encode_plus(
+        [question['NNQT_question']], return_tensors="pt", padding=True, truncation=True
+    )
+    input_ids = inputs_dict.input_ids.to(device)
+    attention_mask = inputs_dict.attention_mask.to(device)
+    outputs = rag_model.generate(  # rag_model overwrites generate
+        input_ids,
+        attention_mask=attention_mask,
+        num_beams=3,
+        min_length=3,
+        max_length=50,
+        n_docs=100,
+        early_stopping=False,
+        num_return_sequences=1,
+        bad_words_ids=[[0, 0]],  # BART likes to repeat BOS tokens, dont allow it to generate more than one
+    )
+    answers = rag_model.retriever.generator_tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
     total_token_count += token_count
     total_questions_with_tokens += 1
