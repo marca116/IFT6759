0bcf5a5c2fac6c2509d118aec94c46f123b47c4c
marca116 <marca116@hotmail.com>
2024-04-14 17:17:08 -0400

    fix

--- query_llm/rag_llm_nomic.py
+++ query_llm/rag_llm_nomic.py
@@ -86,10 +86,10 @@ output_solved_answers_filepath = root_results_folder + "/" + current_time + "_" 
 with open(input_dataset_filename, 'r', encoding='utf-8') as file:
     questions = json.load(file)
 
-# Need to take care better of answer format/type: DATES!
-if dataset_name == "qald_10_train":
-    questions[101]['solved_answer'] = ['1888']
-    questions[101]['answer'] = ['1888']
+# # Need to take care better of answer format/type: DATES!
+# if dataset_name == "qald_10_train":
+#     questions[101]['solved_answer'] = ['1888']
+#     questions[101]['answer'] = ['1888']
 
 # Todo: save this to disk
 encoded_questions = dict()
